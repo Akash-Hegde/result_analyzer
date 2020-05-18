609b69359cbd2ac74674129a05ed4944f2400449
Akash Hegde <akash99.hegde@gmail.com>
2020-05-18 16:03:29 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {
+  Route,
+  NavLink,
+  BrowserRouter as Router,
+  Switch
+} from "react-router-dom";
+import Result from "./Result";
+import ExcelReader from "./ExcelReader"
 
+const routing = (
+  <Router>
+    <div>
+    {/* <Link exact activeClassName="active" to="/">
+      Home
+          </Link> */}
+      
+      
+        <Route exact path="/" component={ExcelReader} />
+        <Route path="/Result" component={Result} />
+        {/* <Route path="/contact" component={Contact} />
+        <Route component={Notfound} /> */}
+      
+    </div>
+  </Router>
+);
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  routing,
   document.getElementById('root')
 );
 
